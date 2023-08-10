0a09209b189c8e53a31c8d2318e19f15f3ac69d7
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-08-10 11:40:54 -0400

    Fixed Exception error and changed stats display.

--- PokemonAPI/Controllers/PokemonController.cs
+++ PokemonAPI/Controllers/PokemonController.cs
@@ -14,10 +14,19 @@ namespace PokemonAPI.Controllers
         }
         public IActionResult RetrievePokemon(string pokemon)
         {
+            JObject pokemonObject;
+
             HttpClient client = new HttpClient();
-            string pokemonURL = $"https://pokeapi.co/api/v2/pokemon/{pokemon.ToLower()}";
-            string pokemonResponse = client.GetStringAsync(pokemonURL).Result;
-            var pokemonObject = JObject.Parse(pokemonResponse);
+            try
+            {
+                string pokemonURL = $"https://pokeapi.co/api/v2/pokemon/{pokemon.ToLower()}";
+                string pokemonResponse = client.GetStringAsync(pokemonURL).Result;
+                pokemonObject = JObject.Parse(pokemonResponse);
+            }
+            catch (Exception ex) 
+            {
+                return RedirectToAction("Index");
+            }
 
             List<string> types = new List<string>();
             foreach(var type in pokemonObject["types"])
@@ -56,100 +65,6 @@ namespace PokemonAPI.Controllers
         public IActionResult Info(PokemonModel newPokemon)
         {
             return View(newPokemon);
-            //Create new instance of class
-            HttpClient client = new HttpClient();
-
-            //User pokemon name input
-            Console.WriteLine("Please enter a pokemon name");
-            var pokemonInput = Console.ReadLine();
-            Console.WriteLine();
-
-            //While loop to get valid input
-            bool conditional = true;
-            var pokemonObject = new JObject();
-
-            while (conditional)
-            {
-                //Try-Catch if input is incorrect
-                try
-                {
-                    //URL endpoint
-                    string pokemonURL = $"https://pokeapi.co/api/v2/pokemon/{pokemonInput.ToLower()}";
-                    //GET request
-                    string pokemonResponse = client.GetStringAsync(pokemonURL).Result;
-                    //Parse Object/Array
-                     pokemonObject = JObject.Parse(pokemonResponse);
-                    //var pokemonObject = JsonConvert.DeserializeObject<PokemonModel>(pokemonResponse);
-
-                    //Write to console
-                    Console.WriteLine("Species:");
-                    Console.WriteLine($"{pokemonObject["species"]["name"]}");
-                    Console.WriteLine();
-
-                    Console.WriteLine($"ID:");
-                    Console.WriteLine($"{pokemonObject["id"]}");
-                    Console.WriteLine();
-
-                    //Foreach loop to write multiple answers if pokemon has more than 1 answer
-                    Console.WriteLine("Type:");
-                    foreach (var type in pokemonObject["types"])
-                    {
-                        Console.WriteLine($"{type["type"]["name"]}");
-                    }
-                    Console.WriteLine();
-
-                    Console.WriteLine("Height:");
-                    Console.WriteLine($"{pokemonObject["height"]} decimeters");
-                    Console.WriteLine();
-
-                    Console.WriteLine("Weight:");
-                    Console.WriteLine($"{pokemonObject["weight"]} hectograms");
-                    Console.WriteLine();
-
-                    //If statement if pokemon doesn't have answer
-                    Console.WriteLine("Possible held items:");
-                    JToken dataToken;
-                    if (pokemonObject.TryGetValue("held_items", out dataToken) && dataToken is JArray array && array.Count == 0)
-                    {
-                        Console.WriteLine("None");
-                    }
-                    else
-                    {
-                        foreach (var items in pokemonObject["held_items"])
-                        {
-                            Console.WriteLine($"{items["item"]["name"]}");
-                        }
-
-                    }
-                    Console.WriteLine();
-
-                    Console.WriteLine("Possible Abilities:");
-                    foreach (var ability in pokemonObject["abilities"])
-                    {
-                        Console.WriteLine($"{ability["ability"]["name"]}");
-                    }
-                    Console.WriteLine();
-
-                    Console.WriteLine("Base Stats:");
-                    foreach (var stats in pokemonObject["stats"])
-                    {
-                        Console.WriteLine($"{stats["stat"]["name"]}: {stats["base_stat"]} ");
-                    }
-                    Console.WriteLine();
-
-                    conditional = false;
-                    //return View(pokemonObject);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Please enter a pokemon name");
-                    string retryInput = Console.ReadLine();
-                    pokemonInput = retryInput;
-                    Console.WriteLine();
-                }
-            }
-
-            return View(pokemonObject);
         }
     }
 }
